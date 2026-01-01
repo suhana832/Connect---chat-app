8241ede024160b93048ad9c3b8dad608b5cc884b
suhana Attar <suhana786attar@gmail.com>
2026-01-01 15:36:53 +0530

    deployment regarding chnages

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -41,5 +41,5 @@ io.on('connection', socket => {
 // Start the server
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
-  console.log('Server is running on port 8000');
+  console.log('Server is running on port ${PORT}');
 });
